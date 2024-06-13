d020f9f94bcf1213bf564cf950d8ec9d6c82c6a5
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-13 19:43:54 +0600

    feat(logo): Implements logo at navbar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../pages/ErrorPage";
+import Home from "../components/Home/Home";
 
 export const router = createBrowserRouter([
     {
@@ -8,7 +9,10 @@ export const router = createBrowserRouter([
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
-
+            {
+                path: "/",
+                element: <Home></Home>
+            }
         ]
     }
 ])
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 // import useAuth from '../../hooks/useAuth';
+import logo from "../../assets/images/logo.png"
 
 const Navbar = () => {
     // const { logOut, user } = useAuth();
@@ -10,23 +11,23 @@ const Navbar = () => {
     // }
 
     return (
-        <div className="navbar bg-white my-3 px-10">
+        <div className="navbar bg-black text-white px-10">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-black text-white rounded-box w-52">
                         <li><Link to={"/"}>Home</Link></li>
                         <li><Link to={"/dashboard"}>Dashboard</Link></li>
                         <li><Link to={"/about"}>About</Link></li>
                         <li><Link to={"/contact"}>Contact</Link></li>
-                        <li><Link className='bg-customBlue rounded-lg' to={"/appointment"}>Appointment</Link></li>
+                        <li><Link className='bg-customBlue rounded-lg' to={"/appointment"}>Task Manager</Link></li>
                         {/* {!user && <li><Link to={"/registration"}>Registration</Link></li>} */}
                     </ul>
                 </div>
                 <Link className="btn btn-ghost text-xl" to="/">
-                    <img className='w-1/2' src="/images/Logo_1.png" alt="Logo" />
+                    <img className='w-1/2 text-lg' src={logo} alt="Logo" />
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
@@ -35,7 +36,7 @@ const Navbar = () => {
                     <li><Link to={"/dashboard"}>Dashboard</Link></li>
                     <li><Link to={"/about"}>About</Link></li>
                     <li><Link to={"/contact"}>Contact</Link></li>
-                    <li><Link className='bg-customBlue rounded-lg' to={"/appointment"}>Appointment</Link></li>
+                    <li><Link className='bg-customBlue rounded-lg' to={"/appointment"}>Task Manager</Link></li>
                 </ul>
             </div>
             <div className="navbar-end space-x-2">
